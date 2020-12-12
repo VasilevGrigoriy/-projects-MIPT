909083fa3052cb9116d3bbd130b7e59aaba306a1
VasilievGrigory <70897555+VasilievGrigory@users.noreply.github.com>
2020-12-13 00:48:08 +0300

    Add files via upload
    Перенес в функцию

--- 1_2_C.cpp
+++ 1_2_C.cpp
@@ -11,7 +11,7 @@ long long partition(std::vector<long long>& arr, long long first, long long end)
     long long target_element = arr[target_index];
     long long i = first;
     long long j = end;
-    while (i!= target_index && j!= target_index)
+    while (i != target_index && j != target_index)
     {
         if (arr[i] > target_element && arr[j] <= target_element)
         {
@@ -46,7 +46,7 @@ long long partition(std::vector<long long>& arr, long long first, long long end)
             --j;
         }
         std::swap(arr[temp - 1], arr[target_index]);
-        return (temp-1);
+        return (temp - 1);
     }
     else if (i != target_index && j == target_index)
     {
@@ -68,41 +68,26 @@ long long partition(std::vector<long long>& arr, long long first, long long end)
         return target_index;
     }
 }
-
-int main()
-{
-    int n1, k1;
-    std::scanf("%d%d", &n1,&k1);
-    long long n = n1, k=k1;
-    long long permament_k = k;
-    std::vector<long long> massive;
-    for (long long i = 0; i < n; ++i)
-    {
-        int x;
-        std::scanf("%d",&x);
-        long long z=x;
-        massive.push_back(z);
-    }
-   
+void main_work(long long& k, long long permament_k, long long n, std::vector<long long>& massive) {
     long long first = 0;
     long long end = n - 1;
-    while (end - first > 1)// Почему бы не выделить и этот цикл в функцию, чтобы сделать main лаконичее?
+    while (end - first > 1)
     {
         long long middle_element = partition(massive, first, end);
         if (permament_k == middle_element)
         {
             std::cout << massive[middle_element];
-            return 0;
+            return;
         }
         else if (k + 1 <= middle_element - first)
         {
-            end = middle_element-1;
+            end = middle_element - 1;
         }
         else
         {
             k = k - middle_element + first - 1;
             first = middle_element + 1;
-            
+
         }
     }
     if (massive[first] > massive[end])
@@ -110,9 +95,22 @@ int main()
         std::swap(massive[first], massive[end]);
     }
     int result = massive[k + first];
-    std::printf("%d",result);
-    
-    return 0;
+    std::cout << result;
 }
 
-
+int main()
+{
+    int n1, k1;
+    std::cin >> n1 >> k1;
+    long long n = n1, k = k1;
+    long long permament_k = k;
+    std::vector<long long> massive;
+    for (long long i = 0; i < n; ++i)
+    {
+        long long x;
+        std::cin >> x;
+        massive.push_back(x);
+    }
+    main_work(k,permament_k,n,massive);
+    return 0;
+}
